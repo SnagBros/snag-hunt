b676ef5a24dbe4efa85e39ba3018152e1d24760c
jay <j33apple@hotmail.com>
2022-02-09 22:01:37 +1100

    Fix build

--- pages/components/Projects.js
+++ pages/components/Projects.js
@@ -19,7 +19,7 @@ export default function Projects() {
   const { data, loading, error } = useQuery(QUERY);
 
   if (loading) {
-    return <h2><a href="#loading" aria-hidden="true" class="aal_anchor" id="loading"></a>Loading...</h2>;
+    return <h2><a href="#loading" aria-hidden="true" id="loading"></a>Loading...</h2>;
   }
 
   if (error) {
@@ -33,7 +33,7 @@ export default function Projects() {
     <div className={styles.grid}>
       {projects.map((project) => (
         <div key={project.name} className={styles.card}>
-          <h3><a href="#project-name" aria-hidden="true" class="aal_anchor" id="project-name"></a>{project.name}</h3>
+          <h3><a href="#project-name" aria-hidden="true" id="project-name"></a>{project.name}</h3>
           <p>
             {project.description.json.content[0].content[0].value} - {project.capacity}
           </p>
